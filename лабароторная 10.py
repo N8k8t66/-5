7ef073d018c424975e79ab994fd16a0d2a4df7a2
N8k8t66 <nikita.nunizyamov@yandex.ru>
2025-11-12 19:44:59 +0400

    Update лабароторная 10.py

--- лабароторная 10.py
+++ лабароторная 10.py
@@ -23,8 +23,8 @@ class Btn():
         self.btn.place(x=x0, y=y0, width=size_btn, height=size_btn)
         self.start_btn = Button(text='Start', height = 2, width=8, command=self.click_start)
         self.start_btn.place(rely=0.9, relx=0.8)
-        self.lbl = Label(text='Cтарт', height=2, width=30)
-        self.lbl.place(relx=0.1, rely=0.9)
+        #self.lbl = Label(text='Старт', height=2, width=30)
+        #self.lbl.place(relx=0.1, rely=0.9)
         self.wins = [[0, 1, 2], [3, 4, 5], [6, 7, 8], [1, 4, 7], [0, 3, 6], [2, 5, 8], [0, 4, 8], [2, 4, 6]]
 
     def unbind1(self, event):
@@ -38,6 +38,52 @@ class Btn():
 
     def cfg_white(self):
         self.btn.config(bg='white')
+
+    def check_best_turn(self):
+        list_combo = []
+        for i in self.wins:
+            for q in btn_comp:
+                if q in i:
+                    list_combo.append(i)
+        for k in btn_click:
+            for x in reversed(list_combo):
+                if k in x:
+                    list_combo.remove(x)
+                    
+        k1 = 0
+        
+        for a1 in range(len(list_combo)):
+        # [0,1,2],[3,4,5]
+        # btn_comb[2,1]
+            for a2 in range(3):
+                if list_combo[a1][a2] in btn_comp:
+                    k1 += 1
+                if k1 == 2:
+                    combo1 = list_combo[a1].copy()
+                    best_turn = reversed(sorted(btn_comp))
+                    for b in best_turn:
+                        if b in combo1:
+                            combo1.remove(b)
+                    return combo1[0]
+            
+            else:
+                k1 = 0
+        k = 0
+        for i in range(len(self.wins)):
+            for j in range(3):
+                if self.wins[i][j] in btn_click:
+                    k += 1
+                if k == 2:
+                    combo = self.wins[i].copy()
+                    contr_turn = reversed(sorted(btn_click))
+                    for z in contr_turn:
+                        if z in combo:
+                            combo.remove(z)
+                    return combo[0]
+                
+            else:
+                k = 0
+        return random.choice(random.choice(list_combo))
 
     def check_win(self, lst1):
         self.win = False
@@ -62,13 +108,15 @@ class Btn():
         self.btn.config(text=str1)
 
     def game(self):
-        while (len(btn_comp) + len(btn_click)) < 9:
+        if len(btn_comp)==0:
             x = random.randint(0, 8)
-            if x not in btn_click and x not in btn_comp:
-                btn_comp.append(x)
-                list_btn[x].cfg_o('0')
-                list_btn[x].unbind1('<Button-1>')
-                break
+        else:
+            x = self.check_best_turn()
+        btn_comp.append(x)
+        list_btn[x].cfg_o('0')
+        list_btn[x].unbind1('<Button-1>')
+                
+            
         self.check_win(btn_comp)
 
     def click_start(self):
