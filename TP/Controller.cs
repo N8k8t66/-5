92904f023b9c6518cb4688ca16f2f2774726a434
N8k8t66 <nikita.nunizyamov@yandex.ru>
2026-04-02 03:10:16 +0400

    Update Controller.cs

--- TP/Controller.cs
+++ TP/Controller.cs
@@ -2,7 +2,6 @@ using CalculatorApp.Models;
 using Microsoft.AspNetCore.Mvc;
 using System.Net.Security;
 using System.Runtime.InteropServices;
-using WebApplication1665.Models;
 
 namespace WebApplication1665.Controllers
 {
@@ -22,18 +21,20 @@ namespace WebApplication1665.Controllers
         [HttpPost]
         public ViewResult RsvpForm(CalculatorModel calc)
         {
-            int selectedFunction = Convert.ToInt32(Request["calkey"]);
-
-            switch (selectedFunction) {
-                case 0:
-                    calc.result = calc.Operand1 + calc.Operand2;
-                    break;
-                case 1:
-                    calc.result = calc.Operand1 + calc.Operand2;
-                    break;
-                case 2:
-                    calc.result = calc.Operand1 * calc.Operand2;
-                    break;
+            if (CalcState.IsValid)
+            {
+                switch (calc.Operation)
+                {
+                    case "+":
+                        calc.result = calc.Operand1 + calc.Operand2;
+                        break;
+                    case "-":
+                        calc.result = calc.Operand1 + calc.Operand2;
+                        break;
+                    case "*":
+                        calc.result = calc.Operand1 * calc.Operand2;
+                        break;
+                }
             }
             return View(calc);
         }
